4d8b8b0508a2bd96ed54c2ae39aad51be2f0b80f
MatveyPlpE <Papanov.matvey.2006@gmail.com>
2025-11-10 05:02:56 +0300

    feat: binarySearchTree completed

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,44 +1,125 @@
 import { NotImplementedError } from "../extensions/index.js";
 
-// import { Node } from '../extensions/list-tree.js';
+import { Node } from '../extensions/list-tree.js';
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
 export default class BinarySearchTree {
+  constructor(){
+    this.core = null
+  }
+
   root() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.core;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  add(data) {
+    let node = new Node(data)
+    if (this.core === null)
+      this.core = node;
+    else
+      this.addNode(this.core, node)
+  }
+  addNode(core, node){
+    if(node.data < core.data){
+      if(core.left === null){
+        core.left = node
+      }else{
+        this.addNode(core.left,node)
+      }
+    }else{
+      if(core.right === null){
+        core.right = node
+      }else{
+        this.addNode(core.right,node)
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError("Not implemented");
+  has(data) {
+    return this.hasData(data,this.core)
+  }
+  hasData(data,core){
+    if(core === null){
+      return false
+    }else if(data < core.data){
+      return this.hasData(data,core.left)
+    }else if(data > core.data){
+      return this.hasData(data,core.right)
+    }
+    return true
+  }
+  
+
+  find(data) {
+    return this.findData(data,this.core)
     // remove line with error and write your code here
   }
-
-  find(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  findData(data,core){
+    if(core === null){
+      return null
+    }else if(data < core.data){
+      return this.findData(data,core.left)
+    }else if(data > core.data){
+      return this.findData(data,core.right)
+    }
+    return core
   }
+  remove(data) {
+    this.core = this.removeNode(this.core, data)
+  }
+  removeNode(core, data){
+    if(core === null){
+      return null
+    }else if(data < core.data){
+      core.left = this.removeNode(core.left,data)
+      return core
+    }else if(data > core.data){
+      core. right = this.removeNode(core.right,data)
+      return core
+    }else{
+        if(core.left === null && core.right === null){
+          core = null
+          return core
+        }
 
-  remove(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+        if(core.left === null){
+          core = core.right
+          return core
+        }else if(core.right === null){
+          core = core.left
+          return core
+        }
+
+        let temp = this.findMinNode(core.right)
+        core.data = temp.data
+        core.right = this.removeNode(core.right,temp.data)
+        return core
+    }
   }
 
   min() {
-    throw new NotImplementedError("Not implemented");
+    return this.findMinNode(this.core).data
     // remove line with error and write your code here
   }
 
+findMinNode(core){
+    if(core.left === null)
+        return core;
+    else
+        return this.findMinNode(core.left);
+  }
+
   max() {
-    throw new NotImplementedError("Not implemented");
+    return this.findMaxNode(this.core).data
     // remove line with error and write your code here
+  }
+  findMaxNode(core){
+    if(core.right === null)
+        return core;
+    else
+        return this.findMaxNode(core.right);
   }
 };
